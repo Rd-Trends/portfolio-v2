0267a366b8c33fc738f78c375570ce55b02641d4
Daniel Ikoyo <dannirolands@gmail.com>
2023-09-27 01:14:00 +0100

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,30 @@ const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Daniel Ikoyo",
-  description: "Full-Stack web developer",
+  description:
+    "Daniel Ikoyo is Full Stack Developer who thrives on delivering high-quality and scalable solutions that meet the needs of his clients.",
+  openGraph: {
+    type: "website",
+    locale: "en_NG",
+    url: "https://dev-rd.vercel.app/",
+    images: "https://dev-rd.vercel.app/og",
+  },
+  twitter: {
+    title: "Daniel Ikoyo",
+    card: "summary_large_image",
+    site: "@Rd_Trendz",
+    creator: "@Rd_Trendz",
+    images: "https://dev-rd.vercel.app/og",
+    description: `Daniel Ikoyo is Full Stack Developer who thrives on delivering high-quality and scalable solutions that meet the needs of his clients.`,
+  },
+  manifest: "/manifest.json",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-icon.png",
+  },
 };
 
 export default function RootLayout({
---
+++ src/app/og/route.tsx
@@ -0,0 +1,38 @@
+import { ImageResponse } from "next/server";
+// App router includes @vercel/og.
+// No need to install it.
+
+export const runtime = "edge";
+
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#141e46",
+          color: "#fff5e0",
+        }}>
+        <h1 style={{ fontSize: 88 }}>Daniel Ikoyo</h1>
+        <p
+          style={{
+            marginTop: 10,
+            fontSize: 32,
+            backgroundColor: "rgb(255 105 105 / 0.2)",
+            color: "#ff6969",
+            padding: 50,
+            paddingTop: 20,
+            paddingBottom: 20,
+            borderRadius: "100",
+          }}>
+          Full Stack Developer
+        </p>
+      </div>
+    )
+  );
+}
